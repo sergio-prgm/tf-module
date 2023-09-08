ac91e8ce7de2fa2235f8686a912d3382a3a98df4
GuilhermeRamosAtos <118266836+GuilhermeRamosAtos@users.noreply.github.com>
2023-09-08 12:50:41 +0100

    Added the imports creation functionality

--- cmd/main.go
+++ cmd/main.go
@@ -38,4 +38,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	resourcesMapping := inout.JsonParser(src + "aztfexportResourceMapping.json")
+	inout.GenerateImports(resourcesMapping, configModules)
 }
